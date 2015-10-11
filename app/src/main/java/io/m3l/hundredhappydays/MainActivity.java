754fef9937a271a69e880df8c970f78639abd07d
Matthew Lancey <git@m3l.io>
2015-10-11 11:45:51 +1100

    Removed the floating action button because it's stupid

--- app/src/main/java/io/m3l/hundredhappydays/MainActivity.java
+++ app/src/main/java/io/m3l/hundredhappydays/MainActivity.java
@@ -3,7 +3,6 @@ package io.m3l.hundredhappydays;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
@@ -12,7 +11,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     public static final int VIEW_PHOTO_REQUEST = 1;
     public static final int VIEW_GALLERY_REQUEST = 2;
     public static final int MEDIA_TYPE_IMAGE = 3;
-    protected Uri mMediaUri;
+    public Uri mMediaUri;
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
      * fragments for each of the sections. We use a
@@ -55,13 +53,13 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 takePictureFragment.newInstance();
             }
-        });
+        });*/
 
     }
 
@@ -91,23 +89,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        // As long as the ActivityResult returned something,
         if (resultCode == RESULT_OK) {
-            // check whether I requested to view the photo.
             if (requestCode == VIEW_PHOTO_REQUEST) {
-                // If I did there is no data in the ActivityResult,
                 if (data == null) {
-                    // show an error.
                     Toast.makeText(this, R.string.general_error, Toast.LENGTH_LONG).show();
-                    // Otherwise, set mMediaUri to be the data that was sent back.
                 } else {
                     mMediaUri = data.getData();
                 }
 
-                // If I actually requested to *take* the photo
             } else {
 
-                // start a new mediaScanIntent so that the
                 Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
                 mediaScanIntent.setData(mMediaUri);
                 sendBroadcast(mediaScanIntent);
